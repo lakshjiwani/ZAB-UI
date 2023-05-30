e2da3100cf02fc32bd5519d69efeee46d85098a3
sameerbatra1 <sameerbatra1@gmail.com>
2023-05-30 23:07:45 +0500

    added validation for registration form

---
+++ Login/login.js
@@ -0,0 +1,35 @@
+var regno = document.getElementById("regnumber");
+var password = document.getElementById("pass");
+
+function validateForm(){
+    // Validating regNumber
+    if(isNaN(regno.value)){
+        console.error("Registration number must be a Integer.");
+        return false;
+    }
+
+    if(regno.value.length !== 7){
+        console.error("Registration number must be a 7-digit number.");
+        return false;
+    }
+
+    // Validating password
+    if(password.value.length < 8){
+        console.error("Password must be 8 characters long");
+        return false;
+    }
+
+    // printing input values on console
+    console.log("Registration Number: " + regno.value);
+    console.log("Password: " + password.value);
+
+    return true;
+}
+
+//Close the menu when a link is clicked
+document.querySelectorAll('.menu a').forEach(link => {
+    link.addEventListener('click', () => {
+      document.getElementById('toggle').checked = false;
+    });
+  });
+  
