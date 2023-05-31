a12818ac73ee27a3e0e19572fb8f041967c9f0d0
sameerbatra1 <sameerbatra1@gmail.com>
2023-06-01 01:32:20 +0500

    Worked on print admit card

--- Home/home.js
+++ Home/home.js
@@ -5,16 +5,20 @@ var uploaded_image = "";
 const toggle = document.getElementById('toggle');
 const menu = document.querySelector('.menu');
 
-toggle.addEventListener('change', function() {
+toggle.addEventListener('change', function () {
   menu.style.display = this.checked ? 'grid' : 'none';
 });
 
-image_input.addEventListener("change", function(){
+image_input.addEventListener("change", function () {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
     uploaded_image = reader.result;
     document.querySelector('#display_image').style.backgroundImage = `url(${uploaded_image})`;
   });
-  
+
   reader.readAsDataURL(this.files[0]);
 });
+
+function printAdmit() {
+  window.print();
+}
