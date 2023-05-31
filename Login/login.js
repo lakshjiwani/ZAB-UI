919df4f42665f98b067c33ce2bfe03008ff32c5a
Mariyum-Dhedhi <maryamdhedhi6@gmail.com>
2023-06-01 03:18:01 +0500

    right iframe implementation

--- Login/login.js
+++ Login/login.js
@@ -1,36 +1,36 @@
 var regno = document.getElementById("regnumber");
 var password = document.getElementById("pass");
 
-function validateForm(){
-    // Validating regNumber
-    if(isNaN(regno.value)){
-        console.error("Registration number must be a Integer.");
-        return false;
-    }
+function validateForm() {
+    // // Validating regNumber
+    // if(isNaN(regno.value)){
+    //     console.error("Registration number must be a Integer.");
+    //     return false;
+    // }
 
-    if(regno.value.length !== 7){
-        console.error("Registration number must be a 7-digit number.");
-        return false;
-    }
+    // if(regno.value.length !== 7){
+    //     console.error("Registration number must be a 7-digit number.");
+    //     return false;
+    // }
 
-    // Validating password
-    if(password.value.length < 8){
-        console.error("Password must be 8 characters long");
-        return false;
-    }
+    // // Validating password
+    // if(password.value.length < 8){
+    //     console.error("Password must be 8 characters long");
+    //     return false;
+    // }
 
-    // printing input values on console
-    console.log("Registration Number: " + regno.value);
-    console.log("Password: " + password.value);
+    // // printing input values on console
+    // console.log("Registration Number: " + regno.value);
+    // console.log("Password: " + password.value);
 
     return true;
 }
 
-function redirect(){
-    if (validateForm() == true){
-        window.location.assign("../ZAB-UI/Home/home.html");
+function redirect() {
+    if (validateForm() == true) {
+        window.location.assign("../Home/home.html");
     }
-    else{
+    else {
         console.log("Cannot redirect");
     }
 }
@@ -38,7 +38,6 @@ function redirect(){
 //Close the menu when a link is clicked
 document.querySelectorAll('.menu a').forEach(link => {
     link.addEventListener('click', () => {
-      document.getElementById('toggle').checked = false;
+        document.getElementById('toggle').checked = false;
     });
-  });
-  
+});
